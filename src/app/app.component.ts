c592db85a904e4c6d827964abb55566a7bd3768d
BOUZIDI Mehdi <mehdi.bouzidi.92@gmail.com>
2018-11-01 20:48:41 +0100

    PART I

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', /* autant que tag*/
+/*  selector: '[app-root]', Autant qu'un élément d'un TAG*/
+/*  selector: '.app-root', Autant que classe d'un TAG*/
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Hello Mehdi';
+
+  contact= {
+    firstName: 'Mehdi',
+    familyName: 'BOUZIDI',
+    email: 'mhd@gmail.com'
+  };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,20 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 
 @NgModule({
+  /* Déclarer les Composants à Utiliser */
   declarations: [
     AppComponent
-  ],
+  ]
+
+  ,
+  /* Importer d'autres Modules */
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
   providers: [],
+  /* Appeler le Composant 'Root' ou le premier Composant qui contient les reste des composants */
   bootstrap: [AppComponent]
 })
 export class AppModule { }
